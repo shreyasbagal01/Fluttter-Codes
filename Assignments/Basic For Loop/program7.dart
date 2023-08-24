a441e1395ca5429567fccf38a0c5969f5b5c8b0f
Shreyas Bagal <112772990+shreyasbagal01@users.noreply.github.com>
2023-08-24 19:33:45 +0530

    Add files via upload

---
+++ Assignments/Basic For Loop/program7.dart
@@ -0,0 +1,7 @@
+void main() {
+  int num1 = 12;
+  for (int i = 1; i <= 10; i++) {
+    int table = num1 * i;
+    print(table);
+  }
+}
---
+++ Assignments/Basic For Loop/program8.dart
@@ -0,0 +1,7 @@
+void main() {
+  int num1 = 12;
+  for (int i = 10; i >= 1; i--) {
+    int table = num1 * i;
+    print(table);
+  }
+}
